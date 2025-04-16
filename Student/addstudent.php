778c3f2eeb49c1b0335dc6099dd3025d6439d7ca
aryanthepain <aryan.gupta@iitg.ac.in>
2025-04-16 11:19:53 +0530

    formatting

--- Student/addstudent.php
+++ Student/addstudent.php
@@ -1,18 +1,17 @@
 <?php
-include_once('../dbconnection.php');
+require_once('../dbconnection.php');
 
-    if(isset($_POST['stusignup']) && isset($_POST['stuname']) && isset($_POST['stuemail']) && isset($_POST['password'])){
+if (isset($_POST['stusignup']) && isset($_POST['stuname']) && isset($_POST['stuemail']) && isset($_POST['password'])) {
 
-        $stuname = $_POST['stuname'];
-        $stuemail = $_POST['stuemail'];
-        $password = $_POST['password'];
+    $stuname = $_POST['stuname'];
+    $stuemail = $_POST['stuemail'];
+    $password = $_POST['password'];
 
-        $sql = "INSERT INTO student(stu_name, stu_email, stu_pass) VALUES ($stuname, $stuemail, $password)";
+    $sql = "INSERT INTO student(stu_name, stu_email, stu_pass) VALUES ($stuname, $stuemail, $password)";
 
-        if($conn->query($sql) == TRUE){
-            echo json_encode('ok');
-        }else{
-            echo json_encode('failed');
-        }
+    if ($conn->query($sql) == TRUE) {
+        echo json_encode('ok');
+    } else {
+        echo json_encode('failed');
     }
-    ?>
+}
